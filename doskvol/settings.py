5a0d3ca8de168004135aa64ff6f4ae13de5371b5
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-07-28 16:39:30 +0200

    basic plumbing for the inner app "generators" ok

--- doskvol/settings.py
+++ doskvol/settings.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
-from pathlib import Path
-
 import environ
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "crispy_forms",
     "crispy_bootstrap4",
+    "generators",
 ]
 
 MIDDLEWARE = [
---
+++ generators/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+app_name = "generators"
+
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
